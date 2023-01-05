42137dadd87b27febf5d2a1553bf9dd34411080a
Roni <roni.kukaj@student.uni-pr.edu>
2023-01-06 00:11:15 +0100

    login changes

---
+++ scripts/login.js
@@ -0,0 +1,18 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "username" && password === "password") {
+        alert("You have successfully logged in.");
+        window.location.href = "home.html";
+    } else {
+        loginErrorMsg.style.color = 'red';
+        loginErrorMsg.style.opacity = 1;
+    }
+})
