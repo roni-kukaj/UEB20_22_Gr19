9bc9a9650e61553410ea2b7ccd4d46c85e56e109
GjemaVisar <visar.gjema@student.uni-pr.edu>
2023-01-16 22:05:08 +0100

    changes

--- scripts/contact_us.js
+++ scripts/contact_us.js
@@ -1,7 +1,25 @@
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("navbar").style.top = "0";
-    } else {
-      document.getElementById("navbar").style.top = "-50px";
-    }
-  }
+let date = new Date();
+let dateString = date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear();
+document.getElementById("date").innerHTML = dateString;
+
+let social_media = ["facebook", "twitter", "googleplus", "linkedin"];
+console.log(social_media[0]);
+console.log(social_media[1]);
+console.log(social_media[2]);
+console.log(social_media[3]);
+
+
+switch social_media{
+  case 0:
+    console.log("Facebook");
+    break;
+  case 1:
+      console.log("Twitter");
+      break;
+  case 2:
+        console.log("GooglePlus");
+        break;
+        case 3:
+          console.log("LinkedIn");
+          break;
+}
