94fc72ef20f8993267d09947daf49dd13c4d000b
GjemaVisar <visar.gjema@student.uni-pr.edu>
2023-01-19 15:51:35 +0100

    minor_changes

--- scripts/contact_us.js
+++ scripts/contact_us.js
@@ -2,11 +2,10 @@ let date = new Date();
 let dateString = date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear();
 document.getElementById("date").innerHTML = dateString;
 
-let social_media = ["facebook", "twitter", "googleplus", "linkedin"];
+const social_media = ["facebook", "twitter", "googleplus", "linkedin"];
 console.log(social_media[0]);
 console.log(social_media[1]);
 console.log(social_media[2]);
 console.log(social_media[3]);
 
 
- 
